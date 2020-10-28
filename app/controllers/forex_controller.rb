00c6f67a3edad9775c422f7b6edf375511c0d522
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-28 19:15:29 +0000

    one link working

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -1,8 +1,20 @@
-class ForexController < ActionController
+class ForexController < ActionController::Base
 
 def forex
+
+  @forex_url = "https://api.exchangerate.host/symbols"
+  raw_data = open(@forex_url).read
+  parsed_data = JSON.parse(raw_data)
+  symbols_array = parsed_data.fetch("symbols").keys
+  # first_result = symbols_array.at(0)
+  # @first_symbol=first_result
+
+# so now that I have the first 
+
+
   render({ :template => "/something_templates/forex.html.erb"})
 end
+
 
 
 
