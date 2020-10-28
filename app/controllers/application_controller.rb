cd815c090ca690d2dd50d964dc677b7bead34323
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-28 18:56:35 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+
+def first
+  render({ :template => "/something_templates/index.html.erb" })
+end
+
+# def forex
+#   render({ :template => "/something_templates/forex.html.erb"})
+# end
+
+
+
+
 end
---
+++ app/controllers/forex_controller.rb
@@ -0,0 +1,10 @@
+class ForexController < ActionController
+
+def forex
+  render({ :template => "/something_templates/forex.html.erb"})
+end
+
+
+
+
+end
